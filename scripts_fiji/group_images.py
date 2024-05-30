41d38dc0da29817613ab180a5a6be0828cf6925e
Zack RIbeiro <ZackRibeiro@gmx.fr>
2024-05-30 11:56:17 +0200

    dsqqsd

--- scripts_fiji/group_images.py
+++ scripts_fiji/group_images.py
@@ -2,6 +2,9 @@ import os, sys
 from ij import IJ, ImagePlus, ImageStack
 from ij.io import FileSaver
 from ij.plugin import Scaler, StackCombiner
+from math import floor
+from ij.gui import Roi
+from java.awt import Rectangle
   
 sourceDir = "/home/irina/Documents/train_need_prepro/masks/"
 targetDir = "/home/irina/Documents/train_need_prepro/outputs/"
@@ -34,26 +37,41 @@ def process(f11,f12,f21,f22):
 		sf21 = f11
 		sf22 = f12
 		
+	RAT = 1.*sf21.width/sf11.width
+	bf11 = sf11.duplicate()
+	bf12 = sf12.duplicate()
+	sf11 = Scaler.resize(sf11, int(sf11.width*RAT), int(sf11.height*RAT), 1, "none")
+	sf12 = Scaler.resize(sf12, int(sf12.width*RAT), int(sf12.height*RAT), 1, "none")
+		
 	if IS_MASKS:
 		toprocess = [sf11,sf12,sf21,sf22]
+		before = [bf11,bf12,sf21.duplicate(),sf22.duplicate()]
 		offset_index = 0
 		for i,mask in enumerate(toprocess):
 			mp = mask.getProcessor()
-			pixels = mp.getPixels()
-			maximum = 0
+			bp = before[i].getProcessor()
+			maximum = offset_index
 			for x in range((mask.width)):
 				for y in range((mask.height)):
-					px_val = mp.getValue(x,y)
-					if(px_val == 0):
-						continue
-					maximum = max(maximum,px_val)
-					mp.putPixel(x,y,int(offset_index+px_val))
-			offset_index += maximum
-		
-		
-	RAT = 1.*sf21.width/sf11.width
-	sf11 = Scaler.resize(sf11, int(sf11.width*RAT), int(sf11.height*RAT), 1, "none")
-	sf12 = Scaler.resize(sf12, int(sf12.width*RAT), int(sf12.height*RAT), 1, "none")
+					b_px_val = 0
+					npxs_val = [0,0,0,0]
+					if i < 2:
+						b_px_val=bp.getValue(int((x/RAT)),int((y/RAT)))
+						npxs_val = [bp.getValue(int((x/RAT)),int((y/RAT))-1),bp.getValue(int((x/RAT)),int((y/RAT))+1),bp.getValue(int((x/RAT))-1,int((y/RAT))),bp.getValue(int((x/RAT))+1,int((y/RAT)))]
+					else:
+						b_px_val=bp.getValue(x,y)
+						npxs_val = [bp.getValue(x,y-1),bp.getValue(x,y+1),bp.getValue(x-1,y),bp.getValue(x+1,y)]
+						
+					if b_px_val == 0:
+						if npxs_val[0] != 0 and npxs_val[0]==npxs_val[1] and npxs_val[1]==npxs_val[2] and npxs_val[2]==npxs_val[3]:
+							print("Pixel modified m:"+str(i+1)+" (x:"+str(x)+" ,y:"+str(y)+"): "+str(offset_index+npxs_val[0]))
+							b_px_val=npxs_val[0]
+						else:
+							mp.putPixel(x,y,0)
+							continue
+					maximum = max(maximum,b_px_val)
+					mp.putPixel(x,y,int(offset_index+b_px_val))
+			offset_index = maximum
 	
 	#Combine
 	scombiner = StackCombiner()
@@ -71,22 +89,49 @@ def process(f11,f12,f21,f22):
 		name += "_mask"
 	fs.saveAsTiff(name+".tif")
 	
-	ITERATION += 1
 	print("Success for " + str(ITERATION))
-		
+	ITERATION += 1
+	
+def cropImg(img,name,crops):
+	if(len(crops) == 0):
+		return img
+	time = int(name.split("_")[1])
+	crop_path = ""
+	for c in crops:
+		if c.split("_")[0] == name.split("_")[0]:
+			crop_path = c
+	crop_file = open(os.path.join(sourceDir,crop_path), "r")
+	
+	x0 = 0
+	y0 = 0
+	x1 = img.width
+	y1 = img.height
+	for i,line in enumerate(crop_file):
+		if(i == time-1):
+			coo = line.split(",")
+			x0 = float(coo[0])
+			y0 = float(coo[1])
+			x1 = float(coo[2])
+			y1 = float(coo[3])
+			break
+	ip = img.getProcessor()
+	ip.setRoi(int(x0),int(y0),int(x1-x0),int(y1-y0));
+	return ImagePlus("cropped_img",ip.crop())
+	
 
 #Create groups
-def group(dataset1, dataset2):
+def group(dataset1, dataset2, crops):
 	imgs1 = []
 	imgs2 = []
 	#Open imgs
 	for f in dataset1:
-		imgs1.append(IJ.openImage(os.path.join(sourceDir, f)))
+		im = IJ.openImage(os.path.join(sourceDir, f))
+		imgs1.append(cropImg(im, f,crops))
 	for f in dataset2:
-		imgs2.append(IJ.openImage(os.path.join(sourceDir, f)))
+		im = IJ.openImage(os.path.join(sourceDir, f))
+		imgs2.append(cropImg(im, f,crops))
 		
-
-	max_groups = min(len(imgs1)-len(imgs1)%2,len(imgs2)-len(imgs2)%2)
+	max_groups = int(floor(min(len(imgs1)-len(imgs1)%2,len(imgs2)-len(imgs2)%2)/2))
 	print("Max Groups possible in this pair: "+str(max_groups))
 	
 	for i in range(max_groups):
@@ -98,9 +143,14 @@ def group(dataset1, dataset2):
 
 def main(folder):
 	datasets = []
+	crops = []
 	#Get files
 	for filename in os.listdir(folder):
 		dset = filename.split("_")[0]
+		if(filename.split("_")[1] == "crop" or filename.split("_")[1] == "crop.txt"):
+			if(not(filename in crops)):
+				crops.append(filename)
+			continue
 		dset_index = containsDataSet(datasets,dset)
 		if(dset_index is None):
 			return
@@ -132,7 +182,7 @@ def main(folder):
 	#Group pairs of datasets
 	i = 0
 	while i < LEN_DATASETS and i+1 < LEN_DATASETS:
-		group(datasets[i],datasets[i+1])
+		group(datasets[i],datasets[i+1],crops)
 		i += 2
 		
 main(sourceDir)
